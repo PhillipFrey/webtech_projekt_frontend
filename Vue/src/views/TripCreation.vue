21edce989419703c3cb4a0db6c98b44613fa3f42
Alexander Bürklen <Alexander.Buerklen@Student.HTW-Berlin.de>
2023-12-13 16:35:45 +0100

    update

--- Vue/src/views/TripCreation.vue
+++ Vue/src/views/TripCreation.vue
@@ -1,32 +1,35 @@
 <template>
 
-  <div class="map-container">
-    <div class="map">
+  <div id="map"></div>
+
+<!--  <div class="map-container">-->
+<!--    <div class="map">-->
 
 
-      <l-map ref="map" v-model:zoom="zoom" v-model:center="center" :useGlobalLeaflet="false">
+<!--      <l-map ref="map" v-model:zoom="zoom" v-model:center="center" :useGlobalLeaflet="false">-->
 
-        <l-tile-layer url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
-                      layer-type="base"
-                      name="Stadia Maps Basemap">
-        </l-tile-layer>
+<!--        <l-tile-layer url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"-->
+<!--                      layer-type="base"-->
+<!--                      name="Stadia Maps Basemap">-->
+<!--        </l-tile-layer>-->
 
-        <l-marker :lat-lng="markerLatLng"></l-marker>
+<!--        <l-marker :lat-lng="markerLatLng"></l-marker>-->
 
-        <l-control-layers position="topright"></l-control-layers>
-        <l-tile-layer
-            v-for="tileProvider in tileProviders"
-            :key="tileProvider.name"
-            :name="tileProvider.name"
-            :visible="tileProvider.visible"
-            :url="tileProvider.url"
-            :attribution="tileProvider.attribution"
-            layer-type="base"/>
+<!--        <l-control-layers position="topright"></l-control-layers>-->
+<!--        <l-tile-layer-->
+<!--            v-for="tileProvider in tileProviders"-->
+<!--            :key="tileProvider.name"-->
+<!--            :name="tileProvider.name"-->
+<!--            :visible="tileProvider.visible"-->
+<!--            :url="tileProvider.url"-->
+<!--            :attribution="tileProvider.attribution"-->
+<!--            layer-type="base"/>-->
 
-      </l-map>
+<!--      </l-map>-->
 
 
-    </div>
+<!--    </div>-->
+  <div>
     <div class="info-panel">
       <h2>Trip Name</h2>
       <p>{{ tripName }}</p>
@@ -43,19 +46,76 @@
   </div>
 </template>
 
-<script setup>
+<!--<script>-->
 
-let markerLatLng = ref([52.5200, 13.4050]);
+<!--// Map initialization-->
+<!--var map = L.map('map').setView([52.5200, 13.4050], 13);-->
 
-import "leaflet/dist/leaflet.css"
-import {LMap, LTileLayer, LMarker, LControlLayers} from "@vue-leaflet/vue-leaflet"
-import { ref } from "vue";
+<!--// osm Layer-->
+<!--var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {-->
+<!--  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'-->
+<!--});-->
 
-let zoom = ref(6)
-let center = ref([52.5200, 13.4050]);
-let tripName = ref('My Trip')
+<!--//osm.addTo(map);-->
 
-</script>
+<!--// OPVN Map-->
+<!--var OPNVKarte = L.tileLayer('https://tileserver.memomaps.de/tilegen/{z}/{x}/{y}.png', {-->
+<!--  maxZoom: 18,-->
+<!--  attribution: 'Map <a href="https://memomaps.de/">memomaps.de</a> <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'-->
+<!--});-->
+<!--// OPNVKarte.addTo(map)-->
+
+
+<!--//GoogleMaps Ansichten-->
+<!--googleSat = L.tileLayer('http://{s}.google.com/vt?lyrs=s&x={x}&y={y}&z={z}',{-->
+<!--  maxZoom: 20,-->
+<!--  subdomains:['mt0','mt1','mt2','mt3']-->
+<!--});-->
+<!--//googleSat.addTo(map);-->
+
+<!--googleStreets = L.tileLayer('http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}',{-->
+<!--  maxZoom: 20,-->
+<!--  subdomains:['mt0','mt1','mt2','mt3']-->
+<!--});-->
+
+<!--googleStreets.addTo(map);-->
+
+
+
+<!--//Marker mit PopUp-->
+<!--var myIcon = L.icon({-->
+<!--  iconUrl: 'markerIcon.png',-->
+<!--  iconSize: [40, 40],-->
+
+<!--});-->
+<!--var singleMarker = L.marker([52.5200, 13.4050],{icon : myIcon, draggable: true});-->
+<!--var popup = singleMarker.bindPopup('Hier sehen Sie das Zentrum von Berlin').openPopup()-->
+<!--popup.addTo(map)-->
+
+<!--//Layer controller listet alle unsere hinzugefügten Maps auf-->
+<!--var baseMaps = {-->
+<!--  "osm": osm,-->
+<!--  'OPNVKarte': OPNVKarte,-->
+<!--  'googleSat' : googleSat,-->
+<!--  'googleStreets' : googleStreets-->
+<!--};-->
+<!--// bezieht sich auf unseren Marker, welcher aktiviert und deaktiviert werden kann-->
+<!--var overlayMaps = {-->
+<!--  "Marker": singleMarker-->
+<!--};-->
+<!--// fügt unsere LayerControl zur Map hinzu-->
+<!--var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);-->
+<!--// let markerLatLng = ref([52.5200, 13.4050]);-->
+<!--//-->
+<!--// import "leaflet/dist/leaflet.css"-->
+<!--// import {LMap, LTileLayer, LMarker, LControlLayers} from "@vue-leaflet/vue-leaflet"-->
+<!--// import { ref } from "vue";-->
+<!--//-->
+<!--// let zoom = ref(6)-->
+<!--// let center = ref([52.5200, 13.4050]);-->
+<!--// let tripName = ref('My Trip')-->
+
+<!--</script>-->
 
 
 
@@ -98,5 +158,31 @@ h3 {
 
 p {
   margin: 0;
+}
+body{
+
+  margin: 0;
+  padding: 0;
+}
+
+
+
+#map{
+  width: 100%;
+  height: 70vh;
+
+}
+
+
+/* Container Design für die Überschrift  */
+.info-container {
+  background-color: #f2f2f2;
+  padding: 20px;
+  text-align: center;
+}
+
+.content {
+  max-width: 80%;
+  margin: 0 auto;
 }
 </style>
