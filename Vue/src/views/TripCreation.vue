30ce37f24ca414451646a6da61391a5a4fcaa1d5
PhillipFrey <Phillip.Frey@Student.HTW-Berlin.de>
2023-12-13 15:15:05 +0100

    welcomePage Design + tripCreation Page update

--- Vue/src/views/TripCreation.vue
+++ Vue/src/views/TripCreation.vue
@@ -1,34 +1,102 @@
 <template>
-  <main>
-  <h1>You made it</h1>
-  <l-map ref="map" v-model:zoom="zoom" v-model:center="center" :useGlobalLeaflet="false">
-    <l-tile-layer url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
-                  layer-type="base"
-                  name="Stadia Maps Basemap"></l-tile-layer>
-  </l-map>
-  </main>
+
+  <div class="map-container">
+    <div class="map">
+
+
+      <l-map ref="map" v-model:zoom="zoom" v-model:center="center" :useGlobalLeaflet="false">
+
+        <l-tile-layer url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+                      layer-type="base"
+                      name="Stadia Maps Basemap">
+        </l-tile-layer>
+
+        <l-marker :lat-lng="markerLatLng"></l-marker>
+
+        <l-control-layers position="topright"></l-control-layers>
+        <l-tile-layer
+            v-for="tileProvider in tileProviders"
+            :key="tileProvider.name"
+            :name="tileProvider.name"
+            :visible="tileProvider.visible"
+            :url="tileProvider.url"
+            :attribution="tileProvider.attribution"
+            layer-type="base"/>
+
+      </l-map>
+
+
+    </div>
+    <div class="info-panel">
+      <h2>Trip Name</h2>
+      <p>{{ tripName }}</p>
+      <div class="info-box">
+        <h3>Marker</h3>
+      </div>
+      <div class="info-box">
+        <h3>Orte</h3>
+      </div>
+      <div class="info-box">
+        <h3>Gesamtdistanz</h3>
+      </div>
+    </div>
+  </div>
 </template>
+
 <script setup>
+
+let markerLatLng = ref([52.5200, 13.4050]);
+
 import "leaflet/dist/leaflet.css"
-import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet"
-import arcades from "./arcades.json"
-import {ref} from "vue";
-
-
+import {LMap, LTileLayer, LMarker, LControlLayers} from "@vue-leaflet/vue-leaflet"
+import { ref } from "vue";
 
 let zoom = ref(6)
-let center = ref([38, 139.69])
-
+let center = ref([52.5200, 13.4050]);
+let tripName = ref('My Trip')
 
 </script>
-<style>
-html, body {
-  margin: 0;
-  padding: 0;
+
+
+
+<style scoped>
+/* Stile für das Layout */
+.map-container {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 20px;
 }
 
-main {
-  height: 100vh;
-  width: 100vw;
+.map {
+  grid-column: 1 / 2;
+  height: 90vh;
+}
+
+.info-panel {
+  grid-column: 2 / 3;
+  display: flex;
+  flex-direction: column;
+}
+
+/* Stile für die grauen Info-Boxen */
+.info-box {
+  background-color: #f0f0f0;
+  border-radius: 8px;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+
+h2 {
+  font-size: 1.5rem;
+  margin-bottom: 10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+  margin-bottom: 5px;
+}
+
+p {
+  margin: 0;
 }
 </style>
--- Vue/src/views/WelcomePage.vue
+++ Vue/src/views/WelcomePage.vue
@@ -1,44 +1,87 @@
 <template>
-  <h1>Welcome to Trip Planer!</h1>
-  <h2>Description of the Application</h2>
-  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
-    magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
-    gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing
-    elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-    At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-    Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-    invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et
-    ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-    Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat
-    nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue
-    duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy
-    nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
-    Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
-    consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore
-    eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril
-    delenit augue duis dolore te feugait nulla facilisi.
-    Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim
-    assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet
-    dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit
-    lobortis nisl ut aliquip ex ea commodo consequat.
-    Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat
-    nulla facilisis.
-    At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-    Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur</p>
-    <h2>Start your journey!</h2>
-    <div>
-      <p>Your trip name is: {{ name }}</p>
-      <input v-model="name" placeholder="Enter Trip name">
+  <div class="container">
+    <h1>Welcome to our Trip Planner!</h1>
+    <div class="description-box">
+      <p class="description">
+        Our trip planner offers you the opportunity to plan your journey, calculate routes from A to B, place markers on the map, and conveniently save your destinations! Start your journey by giving your trip a name
+      </p>
     </div>
-  <router-link to="/TripCreation">
-    <button>Create Trip</button>
-  </router-link>
+    <div class="start-journey">
+      <div class="trip-name">
+        <p>Your trip name is: {{ name }}</p>
+        <input v-model="name" placeholder="Enter Trip name">
+        <router-link to="/TripCreation">
+          <button>Create Trip</button>
+        </router-link>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-
+export default {
+  data() {
+    return {
+      name: ''
+    };
+  }
+};
 </script>
 
-<style>
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  padding: 20px;
+  font-family: 'Arial', sans-serif;
+  text-align: center;
+}
 
+h1 {
+  font-size: 2.5rem;
+  margin-bottom: 20px;
+}
+
+.description-box {
+  background-color: #f0f0f0;
+  border-radius: 8px;
+  padding: 20px;
+  margin-bottom: 20px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+
+  /* Box eingrenzen */
+  max-width: 800px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+p.description {
+  margin: 0;
+}
+
+input {
+  padding: 8px;
+  margin-bottom: 15px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  width: 250px;
+}
+
+button {
+  padding: 10px 10px;
+  font-size: 1rem;
+  border: none;
+  border-radius: 5px;
+  background-color: #4CAF50;
+  color: white;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+button:hover {
+  background-color: #45a049;
+}
 </style>
