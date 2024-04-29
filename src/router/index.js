6041c640be800284c1b642d54e73768419c73c84
Alexander Bürklen <Alexander.Buerklen@Student.HTW-Berlin.de>
2024-04-29 22:21:12 +0200

    changed router

--- src/router/index.js
+++ src/router/index.js
@@ -1,50 +1,41 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import {createRouter, createWebHistory} from 'vue-router';
 import WelcomePage from '../views/WelcomePage.vue';
 import TripCreation from '../views/TripCreation.vue';
 import Fallback from "../views/Fallback.vue";
 
-const routes = [
-  {
-    path: '/',
-    name: 'welcomePage',
-    component: WelcomePage,
-    meta: {
-      title: 'Trip Planner - Welcome Page'
-    }
-  },
-  {
-    path: "/TripCreation/:id",
-    name: "tripCreation",
-    component: TripCreation,
-    meta: {
-      title: 'Trip Planner - Trip Creation'
-    }
-  },
-  {
-    path: "/Fallback",
-    name: "fallback",
-    component: Fallback
-  },
-  {
-    path: '/:pathMatch(.*)*',
-    redirect: '/'
-  }
-];
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  routes: [
+    {
+      path: '/',
+      name: 'welcomePage',
+      component: WelcomePage,
+      meta: {
+        title: 'Trip Planer - Welcome Page'
+      }
+    },
+    {
+      path: "/TripCreation/:id",
+      name: "tripCreation",
+      component: TripCreation,
+      meta: {
+        title: 'Trip Planer - Trip Creation'
+      }
+    },
+    {
+      path: "/Fallback",
+      name: "fallback",
+      component: Fallback
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      redirect: '/'
+    }
+  ]
 });
 
-router.beforeEach((to, from, next) => {
-  const exists = routes.some(route => route.path === to.path);
-
-  if (!exists) {
-    next({ name: 'fallback' });
-  } else {
-    document.title = to.meta?.title ?? 'Default Title';
-    next();
-  }
-});
+router.beforeEach((to) => {
+  document.title = to.meta?.title ?? 'Default Title';
+})
 
 export default router;
